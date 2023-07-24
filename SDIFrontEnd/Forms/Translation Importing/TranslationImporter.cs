9d792ec463ee9e42d5865db8728b932bf5811ce7
Edward Bauer <e3bauer@uwaterloo.ca>
2023-07-24 10:50:05 -0400

    Minor fixes

--- SDIFrontEnd/Forms/Translation Importing/TranslationImporter.cs
+++ SDIFrontEnd/Forms/Translation Importing/TranslationImporter.cs
@@ -59,6 +59,9 @@ namespace SDIFrontEnd
         bool errorsExist;
         #endregion
 
+        int narrowWidth = 560;
+        int wideWidth = 1045;
+
         public TranslationImporter()
         {
             InitializeComponent();
@@ -1068,13 +1071,11 @@ namespace SDIFrontEnd
         {
             if (!Expanded)
             {
-                resultsPanel.Width = 10;
-                this.Width = 550  + 10;
+                this.Width = narrowWidth;
             }
             else
             {
-                resultsPanel.Width = 430 +  10;
-                this.Width = 910;
+                this.Width = wideWidth;
             }
 
             //Expanded = !Expanded;
